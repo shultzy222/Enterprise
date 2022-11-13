cdcebd7789d37a805d5b7a8283ab397c658a3876
shultzy222 <34283579+shultzy222@users.noreply.github.com>
2022-11-12 18:18:36 -0700

    Update API.py

--- API.py
+++ API.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
-import requests
-import json
+import requests, json
+
 api_url = "http://ipwho.is/8.8.4.4"
 response = requests.get(api_url)
 data = response.text
-json.loads(data)
+parse_json = json.loads(data)
+print ("Response: ",(parse_json))
+info = parse_json["postal"]
+print ("Postal code: " + info)
